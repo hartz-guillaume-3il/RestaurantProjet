8e70cb13ea8e7f5991a61920e550c12f66180829
rahou <rahouh@3il.fr>
2025-06-04 12:16:33 +0200

    test

--- src/factory/MenuItemFactory.java
+++ src/factory/MenuItemFactory.java
@@ -6,25 +6,8 @@ import model.Ingredient;
 import java.util.List;
 
 public class MenuItemFactory {
-<<<<<<< Updated upstream
-=======
 	
     // Méthode qui décide quelle factory appeler en fonction du type
-    public static MenuItem createMenuItem(String type, String nom, double prix, String description,
-                                          boolean flag, List<Ingredient> ingredients) {
-        switch (type.toLowerCase()) {
-            case "plat":
-                return PlatFactory.createItem(nom, prix, description, flag, ingredients);
-            case "dessert":
-                return DessertFactory.createItem(nom, prix, description, flag, ingredients);
-            case "entree":
-                return EntreeFactory.createItem(nom, prix, description, flag, ingredients);
-            default:
-                throw new IllegalArgumentException("Type d’élément inconnu : " + type);
-        }
-    }
->>>>>>> Stashed changes
-
 	public static MenuItem createMenuItem(String type, String nom, double prix, String description, boolean flag,
 			List<Ingredient> ingredients) {
 		switch (type.toLowerCase()) {
